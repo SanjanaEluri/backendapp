9706d2cb8d99dc1a84e81de9850ea7eebcc22393
SanjanaEluri <sanjananagamani2004@gmail.com>
2024-05-01 12:12:27 +0530

    deleted  display in menucontroller

--- controllers/menucontroller.js
+++ controllers/menucontroller.js
@@ -3,9 +3,7 @@ const Menuseeker = require('../models/menuseeker');
 
 
 
-const display = (request, response) => {
-  response.send('<h3>Backend is running successfully!</h3>');
-};
+
 
 const insertmenu = async (request, response) => {
   try {
